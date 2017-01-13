12d7001a3114b7b17be12b82a18802bfb2e03e57
Andre <Andre@DESKTOP-EVCUACT>
2017-01-13 19:46:19 -0200

    Compatibility changes

--- wi.core.tabs.events.js
+++ wi.core.tabs.events.js
@@ -1,10 +1,10 @@
 /**
- *  __          __  _    _____ _____  ______ 
- *  \ \        / / | |  |_   _|  __ \|  ____|
- *   \ \  /\  / /__| |__  | | | |  | | |__   
- *    \ \/  \/ / _ \ '_ \ | | | |  | |  __|  
- *     \  /\  /  __/ |_) || |_| |__| | |____ 
- *      \/  \/ \___|_.__/_____|_____/|______|
+ *  __          __  _    _____ _____  ______ _______    _         
+ *  \ \        / / | |  |_   _|  __ \|  ____|__   __|  | |        
+ *   \ \  /\  / /__| |__  | | | |  | | |__     | | __ _| |__  ___ 
+ *    \ \/  \/ / _ \ '_ \ | | | |  | |  __|    | |/ _` | '_ \/ __|
+ *     \  /\  /  __/ |_) || |_| |__| | |____ _ | | (_| | |_) \__ \
+ *      \/  \/ \___|_.__/_____|_____/|______(_)|_|\__,_|_.__/|___/  
  *                                                                            
  *  @author André Ferreira <andrehrf@gmail.com>
  *  @license MIT
@@ -20,6 +20,10 @@
          */
         itens: {},
         
+        /**
+         * Tabs index
+         * @type array
+         */
         tabIndex: [],
         
         /**
@@ -41,7 +45,7 @@
                 tabListItem.id = "wi-tl-" + id;
                 tabListItem.className = "wi-tabs-list-item animated slideInUp";
                 tabListItem.innerHTML = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg"><defs><symbol id="topleft" viewBox="0 0 214 29" ><path d="M14.3 0.1L214 0.1 214 29 0 29C0 29 12.2 2.6 13.2 1.1 14.3-0.4 14.3 0.1 14.3 0.1Z"/></symbol><symbol id="topright" viewBox="0 0 214 29"><use xlink:href="#topleft"/></symbol><clipPath id="crop"><rect class="mask" width="100%" height="100%" x="0"/></clipPath></defs><svg width="50%" height="100%" transfrom="scale(-1, 1)"><use xlink:href="#topleft" width="214" height="29" class="chrome-tab-background"/><use xlink:href="#topleft" width="214" height="29" class="chrome-tab-shadow"/></svg><g transform="scale(-1, 1)"><svg width="50%" height="100%" x="-100%" y="0"><use xlink:href="#topright" width="214" height="29" class="chrome-tab-background"/><use xlink:href="#topright" width="214" height="29" class="chrome-tab-shadow"/></svg></g></svg>'+
-                                        '<div class="wi-tabs-list-item-title">'+title+"</div>";
+                                        '<div class="wi-tabs-list-item-title" title="' + path + '">' + title + "</div>";
                                 
                 tabListItem.onclick = function () { 
                     webide.tabs.focus(id); 
@@ -104,14 +108,11 @@
                         _this.focus(id);
                         
                         var settings = webide.settings.getByPattern(/^ace\.editor\..*?$/i);
-                        var theme = webide.settings.get("ace.editor.theme");
-                        
-                        if(!theme)
-                            theme = "ace/theme/twilight";
-                        else
-                            theme = "ace/theme/" + theme;
+                        var theme = webide.settings.get("ace.editor.theme");                        
+                        theme = (!theme) ? "ace/theme/twilight" : "ace/theme/" + theme;
 
                         var editor = ace.edit("wi-ed-" + id);
+                        //$(".ace_scroller").mCustomScrollbar({theme:"inset"});
                         editor.setTheme(theme);
                         
                         for(var key in settings){
@@ -130,7 +131,7 @@
                         editor.resize();
                         
                         if(typeof cb == "function")
-                            cb("wi-ed-" + id, editor);
+                            cb(id, editor);
                     break;
                     case "url":
                         tabListContents.innerHTML = "<div id='wi-url-" + id + "'></div>";  
@@ -139,11 +140,11 @@
 
                         webide.getContents("GET", path, null, function(data){
                             $("#wi-url-" + id).html(data);
-                            $(".wi-scrollbar").mCustomScrollbar({set_height: "100%", theme:"inset"});
+                            //$(".wi-scrollbar").mCustomScrollbar({set_height: "100%", theme:"inset"});
                             webide.forms.bind();
                             
                             if(typeof cb == "function")
-                                cb("wi-url-" + id);
+                                cb(id);
                         });
                     break;
                 }
@@ -151,6 +152,10 @@
             else{
                 this.focus(id);
             }
+        },
+        
+        addToolbar: function(id){   
+            $("#wi-tc-" + id).append("<div class='wi-tabs-toolbar'></div>");
         },
         
         /**
