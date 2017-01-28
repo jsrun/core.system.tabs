1533672d9b446deda53965adb9774cecdbbb9443
Andre Ferreira <andrehrf@gmail.com>
2017-01-28 06:49:40 -0200

    include gettitle, settitle, remove, getativetab functions

--- events.js
+++ events.js
@@ -23,47 +23,11 @@ webide.module("tabs", function(){
     //Bind onclose tab
     tabsLayout.on('tabCreated', function(container){
         container.closeElement.off('click').click(function(){
-            if(confirm( 'really close this?' )){
-                webide.tabs.remove(container.contentItem.container.id);
+            webide.tabs.remove(container.contentItem.container.id, function(){
                 container.contentItem.remove();
-            }
+            });
         });
-    });
-        
-    //Register editor type
-    tabsLayout.registerComponent('editor', function(container, state){
-        container.id = state.id;
-        container.getElement().html("<div id='wi-ed-" + state.id + "'></div>");     
-        webide.tabs.itens[state.id].container = container;
-                
-        setTimeout(function(){
-            var settings = webide.settings.getByPattern(/^ace\.editor\..*?$/i);
-            var theme = webide.settings.get("ace.editor.theme");                        
-            theme = (!theme) ? "ace/theme/twilight" : "ace/theme/" + theme;
-
-            var editor = ace.edit("wi-ed-" + state.id);
-            editor.setTheme(theme);
-            editor.setOptions({enableBasicAutocompletion: true, enableSnippets: true, enableLiveAutocompletion: false});
-
-            for(var key in state.settings){
-                if(key !== "ace.editor.theme"){
-                    if(state.settings[key] == "true") state.settings[key] = true;//Bugfix
-                    if(state.settings[key] == "false") state.settings[key] = false;//Bugfix
-
-                    if(!isNaN(parseInt(state.settings[key])))
-                        state.settings[key] = parseInt(state.settings[key]);
-
-                    editor.setOption(key.replace(/ace\.editor\./img, ""), state.settings[key]);
-                }
-            }
-
-            webide.tabs.itens[state.id].editor = editor;
-            editor.resize();
-
-            if(typeof webide.tabs.itens[state.id].cb == "function")
-                setTimeout(function(state, editor){ webide.tabs.itens[state.id].cb(state.id, editor); }, 300, state, editor);
-        }, 100);
-    });
+    });        
     
     //Register url type
     tabsLayout.registerComponent('url', function(container, state){
@@ -78,17 +42,7 @@ webide.module("tabs", function(){
             if(typeof webide.tabs.itens[state.id].cb == "function")
                 setTimeout(function(state){ webide.tabs.itens[state.id].cb(state.id); }, 300, state);
         });
-    });
-    
-    //Register stream type
-    tabsLayout.registerComponent('stream', function(container, state){
-        container.id = state.id;
-        container.getElement().html("<div id='wi-ed-" + state.id + "'></div><div id='wi-stream-" + state.id + "'></div>");
-        webide.tabs.itens[state.id].container = container;
-        
-        if(typeof webide.tabs.itens[state.id].cb == "function")
-            setTimeout(function(state){ webide.tabs.itens[state.id].cb(state.id); }, 300, state);
-    });
+    });    
 
     tabsLayout.init();
 
@@ -110,14 +64,16 @@ webide.module("tabs", function(){
          * @param string title
          * @param string path
          * @param string type
-         * @param string language
+         * @param string settings
+         * @param function cb
+         * @param function onclose
          * @return void
          */
-        add: function(title, path, type, settings, cb){
+        add: function(title, path, type, settings, cb, onclose){
             var id = this.createNamespace(path);
             
             if(!this.has(id)){
-                this.itens[id] = {path: path, type: type, settings: settings, cb: cb};
+                this.itens[id] = {path: path, type: type, settings: settings, cb: cb, onclose: onclose};
                 
                 if(!this.layout.root.contentItems[0])
                     this.layout.root.addChild({type: 'stack'});
@@ -127,7 +83,8 @@ webide.module("tabs", function(){
                     title: title,
                     type: 'component',
                     componentName: type,
-                    componentState: {id: id, path: path, settings: settings, cb: cb}
+                    tooltip: path,
+                    componentState: {id: id, path: path, settings: settings, cb: cb, onclose: onclose}
                 };
                 
                 for(var key in settings)
@@ -139,6 +96,50 @@ webide.module("tabs", function(){
                this.focus(id);
             }
         },
+        
+        /**
+         * Function to get title if exists
+         * 
+         * @param string id
+         * @return string
+         */
+        getTitle: function(id){
+            if(this.layout.root.contentItems[0])
+                return (this.layout.root.contentItems[0].getItemsById(id).length > 0) ? this.layout.root.contentItems[0].getItemsById(id)[0].config.title : "";
+            else
+                return "";
+        },
+        
+        /**
+         * Function to set title
+         * 
+         * @param string id
+         * @param string newtitle
+         * @return boolean
+         */
+        setTitle: function(id, newtitle){
+            if(this.layout.root.contentItems[0]){
+                if(this.layout.root.contentItems[0].getItemsById(id).length > 0){
+                    this.layout.root.contentItems[0].getItemsById(id)[0].setTitle(newtitle);
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        },
+        
+        /**
+         * Function to return actived tab
+         * 
+         * @return object|null
+         */
+        getActiveTab: function(){
+            return (this.layout.root.contentItems[0]) ? this.layout.root.contentItems[0].getActiveContentItem() : null;
+        },
        
         /**
          * Functio to remove tab
@@ -146,9 +147,33 @@ webide.module("tabs", function(){
          * @param string id
          * @return void
          */
-        remove: function(id){
-            if(this.has(id))
-                this.itens[id] = null;
+        remove: function(id, fn){
+            if(typeof this.itens[id].onclose == "function"){
+                if(this.itens[id].onclose(id)){
+                    if(this.has(id)){
+                        this.itens[id] = null;
+                                            
+                        if(this.layout.root.contentItems[0])
+                            if(this.layout.root.contentItems[0].getItemsById(id).length > 0)
+                                this.layout.root.contentItems[0].getItemsById(id)[0].remove();
+                        
+                        if(typeof fn == "function")
+                            fn();
+                    }
+                }
+            }
+            else{
+                if(this.has(id)){
+                    this.itens[id] = null;
+                        
+                    if(this.layout.root.contentItems[0])
+                        if(this.layout.root.contentItems[0].getItemsById(id).length > 0)
+                            this.layout.root.contentItems[0].getItemsById(id)[0].remove();
+                    
+                    if(typeof fn == "function")
+                        fn();
+                }
+            }
         },
                
         /**
@@ -180,12 +205,8 @@ webide.module("tabs", function(){
          */
         focus: function(id){
             if(this.has(id)){
-                console.log(this.itens[id].container);
-                this.layout.selectItem(this.itens[id].container);
-                /*$(".wi-tabs-list-item-active").removeClass("wi-tabs-list-item-active");
-                $(".wi-tabs-contents-item-active").removeClass("wi-tabs-contents-item-active");
-                $("#wi-tl-" + id).addClass("wi-tabs-list-item-active");
-                $("#wi-tc-" + id).addClass("wi-tabs-contents-item-active");*/
+                if(this.layout.root.contentItems[0].getItemsById(id).length > 0)
+                    this.layout.root.contentItems[0].setActiveContentItem(this.layout.root.contentItems[0].getItemsById(id)[0]);
             }
         },
         
